b3db63eac6dce175a95480399cc90ce982ed65d1
tljuniper <48209000+tljuniper@users.noreply.github.com>
2022-12-07 08:33:19 +0100

    raspi: Make config similar to others

--- hosts/raspi/configuration.nix
+++ hosts/raspi/configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, lib, ... }:
 {
   nix = {
@@ -14,6 +18,10 @@
  
   # boot.kernelPackages = pkgs.linuxPackages_latest;
 
+
+  networking.hostName = "rust";
+
+  # Set your timeZone
   time.timeZone = "Europe/Berlin";
   
   # File systems configuration for using the installer's partition layout
@@ -32,21 +40,41 @@
   };
 
   environment.systemPackages = with pkgs; [
-    vim
-    git
     bat
-    wget
+    git
     minicom
-    usbutils
     tree
+    unixtools.netstat
+    usbutils
+    vim
+    wget
   ];
 
-  services.openssh.enable = true;
 
   hardware.enableRedistributableFirmware = true;
 
-  system.stateVersion = "22.05";
+  # List the services that you want to enable:
 
-  networking.hostName = "rust";
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
