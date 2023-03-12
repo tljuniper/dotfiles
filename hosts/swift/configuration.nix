40c723c666ee6309c4cb1ee6ff2480c4dc29968c
Juniper <48209000+tljuniper@users.noreply.github.com>
2023-03-12 08:07:55 +0100

    Update swift

--- hosts/swift/configuration.nix
+++ hosts/swift/configuration.nix
@@ -15,6 +15,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
+
   networking.hostName = "swift"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
