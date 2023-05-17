b4d23c4e9453102bceacd964a958b23725dd54c5
tljuniper <48209000+tljuniper@users.noreply.github.com>
2023-05-17 10:51:25 +0200

    Blazer: Remove old nix cache

--- hosts/blazer/nix-cache.nix
+++ hosts/blazer/nix-cache.nix
@@ -4,12 +4,10 @@
   nix.settings = {
     substituters = [
       "http://binary-cache-v2.vpn.cyberus-technology.de"
-      "https://binary-cache.vpn.cyberus-technology.de"
       "https://cache.nixos.org"
     ];
     trusted-public-keys = [
       "binary-cache.vpn.cyberus-technology.de:qhg25lVqyCT4sDOqxY6GJx8NF3F86eAJFCQjZK/db7Y="
-      "cyberus-1:0jjMD2b+guloGW27ZToxDQApCoWj+4ONW9v8VH/Bv0Q="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
