c238e6c05ab8f005f33afb2075229362f84d2933
Juniper <48209000+tljuniper@users.noreply.github.com>
2026-01-08 19:40:45 +0100

    fix: git settings

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -16,20 +16,20 @@
           switchMr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
         };
 
-        extraConfig = {
-          pager = {
-            branch = false;
-          };
-          core = {
-            editor = "vim";
-          };
-          init = {
-            defaultBranch = "main";
-          };
-          interactive.diffFilter = "${pkgs.diff-so-fancy}/bin/diff-so-fancy --patch";
-          # Push new branch to origin (or default branch) without explicit --set-upstream
-          push.autoSetupRemote = true;
+        # extraConfig = {
+        # };
+        pager = {
+          branch = false;
         };
+        core = {
+          editor = "vim";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        interactive.diffFilter = "${pkgs.diff-so-fancy}/bin/diff-so-fancy --patch";
+        # Push new branch to origin (or default branch) without explicit --set-upstream
+        push.autoSetupRemote = true;
       };
       ignores = [ ".vscode/" "*.pem" ];
     };
